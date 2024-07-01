a291cd1335e6e6d8b81394f109d52a0eb6dd75f5
Albano Andrés González <164359863+band-width@users.noreply.github.com>
2024-07-01 11:51:28 -0300

    Add files via upload

---
+++ app-intro/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './header';
+import Body from './body';
+import Footer from './footer';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
+
---
+++ app-intro/src/body.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Body = () => {
+  return (
+    <main>
+      <p>Body</p>
+    </main>
+  );
+};
+
+export default Body;
